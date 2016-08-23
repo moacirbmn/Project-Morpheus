ec74ecf6b4d3ea024da2763276007f8c37a428d8
Andre Matte <carlosandrematte@gmail.com>
2016-08-23 20:29:24 -0300

    H Bridge Test Code

---
+++ TEST_HBridge/TEST_HBridge.ino
@@ -0,0 +1,50 @@
+#define Stop            1
+#define MoveForward     2
+#define MoveBackwards   3
+#define TurnLeft        4
+#define TurnRight       5
+
+// ---- Componente      Porta ----
+int Relay1         = 3;
+int Relay2         = 4;
+int Relay3         = 5;
+int Relay4         = 6;
+
+int Movement = 0;
+int HBridge [4];
+
+
+// --- Functions ---
+
+void Move ()
+{
+  switch (Movement)
+  {
+    case Stop:          HBridge[0] = 0; HBridge[1] = 0; HBridge[2] = 0; HBridge[3] = 0; break;
+    case MoveForward:   HBridge[0] = 1; HBridge[1] = 0; HBridge[2] = 1; HBridge[3] = 0; break;
+    case MoveBackwards: HBridge[0] = 0; HBridge[1] = 1; HBridge[2] = 0; HBridge[3] = 1; break;
+    case TurnLeft:      HBridge[0] = 1; HBridge[1] = 0; HBridge[2] = 0; HBridge[3] = 1; break;
+    case TurnRight:     HBridge[0] = 0; HBridge[1] = 1; HBridge[2] = 1; HBridge[3] = 0; break;
+  }
+
+  digitalWrite(Relay1, HBridge[0]);
+  digitalWrite(Relay2, HBridge[1]);
+  digitalWrite(Relay3, HBridge[2]);
+  digitalWrite(Relay4, HBridge[3]);
+}
+
+
+// --- Main Functions ---
+
+void setup () 
+{
+  pinMode (Relay1, OUTPUT);
+  pinMode (Relay2, OUTPUT);
+  pinMode (Relay3, OUTPUT);
+  pinMode (Relay4, OUTPUT);
+}
+
+void loop ()
+{
+  
+}
